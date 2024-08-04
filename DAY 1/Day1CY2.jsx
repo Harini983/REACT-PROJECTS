8461b6808ebb7e422e45a60328724c65917f79b7
Harini983 <150882480+Harini983@users.noreply.github.com>
2024-08-04 21:09:30 +0530

    Add files via upload

---
+++ DAY 1/Day1CY2.jsx
@@ -0,0 +1,13 @@
+function array(){
+    let arr1=["North","West","East","South"];
+
+}
+
+function Ap(){
+    return(
+        <div className="Ap">
+            <button onClick={array}>array</button> 
+        </div>
+    )
+}
+export default Ap;
